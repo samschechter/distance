b85e11d6b88a8b6b23817e6633e4d924c31e4b89
Tom Collier <collier@apartmentlist.com>
2017-04-25 13:02:14 -0700

    Handle 2.3 vs 2.4 Fixnum/Integer difference

--- lib/distance/core_ext/integer.rb
+++ lib/distance/core_ext/integer.rb
@@ -1,7 +1,36 @@
-class Integer
-  alias_method :orig_multiply, :*
+# Test to see if Fixnum/Bignum are deprecated, which implies we are in ruby 2.4
+# or greater
 
-  def *(other)
-    other.is_a?(Distance) ? other * self : orig_multiply(other)
+# Silence any deprecation warnings
+warn_level = $VERBOSE
+$VERBOSE = nil
+v2_4 = Fixnum == Integer
+$VERBOSE = warn_level
+
+if v2_4
+  class Integer
+    alias_method :orig_multiply, :*
+
+    def *(other)
+      other.is_a?(Distance) ? other * self : orig_multiply(other)
+    end
+  end
+else
+  # In ruby versions before 2.4, multiplication was defined in Bignum and Fixnum,
+  # not in Integer
+  class Bignum
+    alias_method :orig_multiply, :*
+
+    def *(other)
+      other.is_a?(Distance) ? other * self : orig_multiply(other)
+    end
+  end
+
+  class Fixnum
+    alias_method :orig_multiply, :*
+
+    def *(other)
+      other.is_a?(Distance) ? other * self : orig_multiply(other)
+    end
   end
 end
